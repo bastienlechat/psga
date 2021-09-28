1ac6c986aa4e55fd8ddcb6088e0490611d121de8
Bastien <bastien.lechat@flinders.edu.au>
2021-09-28 10:03:10 +0930

    test

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='psga',
+    version='0.0.1',
+    packages=['psga'],
+    url='',
+    license='',
+    author='Bastien Lechat',
+    author_email='bastien.lechat@flinders.edu.au',
+    description=''
+)
